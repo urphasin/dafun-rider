a2ae6b5a408fc3f3d32fb61d1e2d65ed59c5699d
otito <otitoamuga@gmail.com>
2025-06-08 16:32:44 -0400

    tailwind bug driving me loose

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,10 +7,9 @@ const Header = () => {
   // const setIsDark = array[1];
 
   return (
-    <header className="bg-white shadow-md px-4 py-3 flex justify-between items-center">
+    <header className="bg-white dark: shadow-md px-4 py-3 flex justify-between items-center">
       <div className="text-[#1F2937] text-xl font-bold">Dafun Riders</div>
       <nav className="flex gap-4">
-        <a className="text-" href=""></a>
         <a className="text-brandPrimaryTextLight hover:text-brandOrange" href="/">Home</a>
         <a className="text-brandPrimaryTextLight hover:text-brandBlue" href="/find">Find a Ride</a>
         <a className="text-brandPrimaryTextLight hover:text-brandBlue" href="/offer">Offer a Ride</a>
